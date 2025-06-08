e30e04a14d5f114ee7d07e28d1659946a9fd522d
Dhruva16630 <dhruva6kumar1@gmail.com>
2025-06-08 19:01:39 +0530

    Added db in package

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,24 @@
 import express from "express";
-
+import z from "zod"
 const app = express();
 
+
+app.post("/signup", (req, res) =>{
+    const requiredBody = z.object({
+        email: z.string().min(3).max(12).email(),
+        password: z.string().min(6).max(10).regex(/[A-Z]/,"Password must contain atleast one uppercase").regex(/[0-9]/,"Password must contain at least one number").regex(/[!@#$%^&*(),.?":{}|<>]/,"Password must contain at least one special character"),
+        firstName: z.string().min(3).max()
+    })
+})
+
+
+app.post("/signin", (req, res) => {
+
+})
+
+
+
+app.post("/room", (req, res) => {
+
+})
 app.listen(3001);
