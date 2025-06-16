e71692b2e9bd3ab9ac05bc174366c0b70ecc07fc
Dhruva16630 <dhruva6kumar1@gmail.com>
2025-06-16 23:28:26 +0530

    Added common_folder in packages

---
+++ packages/common/src/types.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+
+export const signupZod = z.object({
+    email: z.string().email(),
+    password: z.string().min(6).max(12).regex(/[A-Z]/,"Password must contain atleast one uppercase").regex(/[0-9]/,"Password must contain at least one number").regex(/[!@#$%^&*(),.?":{}|<>]/,"Password must contain at least one special character"),
+    username: z.string().min(3).max(10)
+});
