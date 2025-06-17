d4636ebef1691a3beab812e9eaf39164ef9ad76c
Dhruva16630 <dhruva6kumar1@gmail.com>
2025-06-17 22:37:03 +0530

    Completed Signup/in request

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -1,8 +1,14 @@
 import { z } from "zod";
 
 
-export const signupZod = z.object({
+
+export const SignupZod = z.object({
     email: z.string().email(),
     password: z.string().min(6).max(12).regex(/[A-Z]/,"Password must contain atleast one uppercase").regex(/[0-9]/,"Password must contain at least one number").regex(/[!@#$%^&*(),.?":{}|<>]/,"Password must contain at least one special character"),
     username: z.string().min(3).max(10)
 });
+
+export const SigninZod = z.object({
+    email: z.string().email(),
+    pasword : z.string().min(6).max(12).regex(/[A-Z]/,"Password must contain atleast one uppercase").regex(/[0-9]/,"Password must contain at least one number").regex(/[!@#$%^&*(),.?":{}|<>]/,"Password must contain at least one special character"),
+})
